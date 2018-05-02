ce0229b50c2d45debf72e71bdb9773d5ac1b0fdd
kanin <kaninhub@hotmail.com>
2018-05-02 03:58:21 +0000

    Update market_demo.sol

--- Solidity_Contract/market_demo.sol
+++ Solidity_Contract/market_demo.sol
@@ -5,11 +5,13 @@ contract market{
     event listAssets(string name,uint  idOwner);
     event listUser(string name,address wallet);
     event showId(uint id);
-
+    
+    uint idAssest=1;
+    uint idUser=1;
     struct Asset{
+        uint _id;
         string name;
-        uint  idOwner;
-        
+        address  idOwner;
         uint price;
         //bool bought;
         // string description;
@@ -17,40 +19,78 @@ contract market{
     Asset[] public assets;
     
     struct User{
-        string name;
         address wallet;
         uint balance;
+     
     }
     User[] public users;
-    function buy (uint _idAssest,uint _idNewOwner){
+    
+    mapping (address => uint[]) public userItemsIds;
+    mapping (uint => address) public itemIdToUser;
+    
+    
+    function buy (uint _idAssest,uint _price){
        //1. check assets that have?
-       require(_idAssest<=assets.length,"didnt have this assets");
+    //   require(_idAssest<=assets.length,"didnt have this assets");
        //2.check money _idNewOwner
-       require((assets[_idAssest].price)<=(users[_idNewOwner].balance),"didnt have money enough");
-        //increase money to _idOldOwner
-        users[assets[_idAssest].idOwner].balance+=assets[_idAssest].price;
-        //decrease money to _idNewOwner
-        users[_idNewOwner].balance-=assets[_idAssest].price;
-        //change owner
-        assets[_idAssest].idOwner=_idNewOwner; 
+  //     require((assets[_idAssest].price)<=((msg.value)),"didnt have money enough");
+       //3.check _price equal assetprice?
+  //     require(assets[_idAssest].price==_price);
+       //increase money to _idOldOwner
+      ////users[assets[_idAssest].idOwner].balance+=assets[_idAssest].price;
+       //decrease money to _idNewOwner
+     /////  users[_idNewOwner].balance-=assets[_idAssest].price;
+      
+       
+       
+       
+       //delete item old owner
+       uint[] memory old= userItemsIds[assets[_idAssest].idOwner];
+       uint[] memory temp = new uint[](old.length-1);
+       
+       uint pointer=temp.length;
+       for(uint i =0;i<temp.length;i++){
+          if(i==_idAssest-1){
+               pointer =i;
+               temp[i]=old[i+1];
+           }else if(i<pointer){
+                temp[i]=old[i];
+               
+           }else if(i>pointer){
+                temp[i]=old[i+1];
+           }
+       }
+            delete (userItemsIds[assets[_idAssest].idOwner]);
+           (userItemsIds[assets[_idAssest].idOwner])=temp;
+       
+     
+      
+       assets[_idAssest].idOwner=msg.sender; 
+       //change owner
+       userItemsIds[msg.sender].push(_idAssest);
+       
+       
     }
-    function createAsset(string _name,uint  _idOwner,uint _price){
-        uint id =assets.push(Asset(_name, _idOwner,_price));
-        showId(id);
+    function createAsset(string _name,uint _price) payable {
+        assets.push(Asset(idAssest,_name, msg.sender,_price));
+        itemIdToUser[idAssest] = msg.sender;
+        userItemsIds[msg.sender].push(idAssest);
+        idAssest++;
     }
-    function createUser(string _name,address _owner,uint _balance){
-        uint id =users.push(User(_name,_owner,_balance));
-        showId(id);
+    function createUser(){
+        users.push(User(msg.sender,msg.value));
+      
     }
-    function listAllAssets() public view {
-        for(uint i =0;i<users.length;i++){
-            listAssets(assets[i].name,assets[i].idOwner);
-        }
+
+    //This function will return list of ids of all items belonging to the _userAddress
+    function getUserItems(address _userAddress) public view returns (uint[] items){
+        return userItemsIds[_userAddress];
     }
-    function listAllUsers() public view{
-        for(uint i =0;i<=users.length;i++){
-            listUser(users[i].name,users[i].wallet);
-        }
+
+    //This function returns all information about single item
+    function getItemInfo(uint _itemId) public view returns (string nameOfItem, address _idOwner, uint price) {
+        Asset memory item = assets[_itemId];
+        return ( item.name, item.idOwner, item.price);
     }
 
 }
