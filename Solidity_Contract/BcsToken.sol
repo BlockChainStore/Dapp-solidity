81a9d719f758a532b733a53b2b33098b36053d42
kanin pathomwattananuruk <kaninhub@hotmail.com>
2018-05-25 17:16:20 +0700

    update digit

--- Solidity_Contract/BcsToken.sol
+++ Solidity_Contract/BcsToken.sol
@@ -4,15 +4,17 @@ import './IERC20.sol';
 
 contract BcsToken is IERC20{
     
-    uint public constant _totalSupply =1000000;
+    uint public constant _totalSupply =1000000*(10**8);
     
     string public constant symbol= "BCS";
     string public constant name ="BCS Token";
-    
+    uint8 public constant decimals=8;
+    address owner;
     mapping(address => uint256) balances;
     mapping(address => mapping(address =>uint256)) allowed;
     
-    function BcsToken(){
+    function FuncToken(){
+        owner=msg.sender;
         balances[msg.sender]= _totalSupply;
     }
     function totalSupply() constant returns (uint256 totalSupply){
@@ -48,6 +50,7 @@ contract BcsToken is IERC20{
     function allowance(address _owner,address _spender) constant returns(uint256 remaining){
         return allowed[_owner][_spender];
     }
+    function kill() { if (msg.sender == owner) selfdestruct(owner); }
     event Transfer(address indexed _from, address indexed _to,uint256 _value);
     event Approval(address indexed _owner, address indexed _spender,uint256 _value);
 }
